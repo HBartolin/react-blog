0ed070c6e98924f86c8283dd1968628bc93da2f1
Hrvoje Bartolin <hbartolin@gmail.com>
2023-09-13 08:54:14 +0200

    1

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState("mario");
+
     const handleClick = () => {
-        console.log("CLIK");
+        setName("lugigy");
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={() => handleClick()}>Click me</button>
+            <p>{name}</p>
         </div>
      );
 }
