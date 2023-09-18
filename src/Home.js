82dbbf76627cd4637cadb468fc8954296d7f829d
Hrvoje Bartolin <hbartolin@gmail.com>
2023-09-18 10:29:01 +0200

    1

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,6 +13,10 @@ const Home = () => {
 
        setBlogs(newBlog);
     }
+
+    useEffect(() => {
+        console.log("E");
+    });
 
     return ( 
         <div className="home">
